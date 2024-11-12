b562e634f941979353bac8203796d36b75d85d85
Leonid1966-dev <rskle66@gmail.com>
2024-11-12 20:49:54 +0300

    Add files via upload

---
+++ Pyhton course.py
@@ -0,0 +1,9 @@
+total_tasks = 12
+hours_spent = 1.5
+course_name = 'Pyhton'
+time_for_one_task = (hours_spent / total_tasks)
+total_tasks = str(total_tasks)
+hours_spent = str(hours_spent)
+time_for_one_task = str(time_for_one_task)
+print('Курс:',course_name + ',', 'всего задач:',total_tasks + ',' , 'затрачено часов:',hours_spent + ',', 'среднее время выполнения:',time_for_one_task + '.')
+
